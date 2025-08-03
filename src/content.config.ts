142f7182433515ed1a3f9fdff08ef7fe73612a6b
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-08-03 17:01:06 +0200

    feat: Work on rest of the pages

---
+++ src/content.config.ts
@@ -0,0 +1,24 @@
+import { defineCollection, z } from 'astro:content';
+import { file, glob } from 'astro/loaders';
+
+const recipes = defineCollection({
+    loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/bread' }),
+    schema: z.object({
+        title: z.string(),
+        slug: z.string(),
+        content: z.string()
+    })
+});
+
+const concerts = defineCollection({
+    loader: file('src/content/concerts.json'),
+    schema: z.object({
+        artist: z.string(),
+        date: z.string().transform((dateString) => new Date(dateString)),
+        location: z.string(),
+        festival: z.string().optional(),
+        id: z.string()
+    })
+});
+
+export const collections = { recipes, concerts };
