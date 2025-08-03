edaa5ebb4d9bbc750fa2fec7d200826400403802
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-08-03 09:49:03 +0200

    chore: Setup tw and add path alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    adapter: cloudflare({
-        imageService: 'compile'
-    }),
+  output: 'static',
+
+  adapter: cloudflare({
+      imageService: 'compile'
+  }),
+
+  vite: {
+    // @ts-expect-error
+    // see issue: https://github.com/withastro/astro/issues/14030
+    plugins: [tailwindcss()],
+  },
 });
