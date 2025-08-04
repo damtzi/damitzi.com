5a3e3e6beea0255515a2d504c05b8c539066ed10
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-08-04 19:29:22 +0200

    chore: Add 'site' for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://damitzi.com',
 
   adapter: cloudflare({
       imageService: 'compile'
