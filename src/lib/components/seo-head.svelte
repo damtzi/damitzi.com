7815ca065b41c029d9e1ad103bda1e98bbb3cae4
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-09-19 16:16:13 +0200

    feat: Create a reusable seo component

---
+++ src/lib/components/seo-head.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { page } from '$app/state';
+    import favicon from '$lib/assets/favicon.svg';
+    let { title, description }: { title: string; description: string } = $props();
+</script>
+
+<svelte:head>
+    <link rel="icon" href={favicon} />
+    <title>{`${title} | damitzi`}</title>
+    <meta name="title" content={title} />
+    <meta name="description" content={description} />
+    <link rel="canonical" href={`${page.url.origin}${page.url.pathname}`} />
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
     import '../app.css';
-    import favicon from '$lib/assets/favicon.svg';
     import { dev } from '$app/environment';
     import { injectAnalytics } from '@vercel/analytics/sveltekit';
+    import SeoHead from '$lib/components/seo-head.svelte';
 
     injectAnalytics({ mode: dev ? 'development' : 'production' });
 
     let { children } = $props();
 </script>
 
-<svelte:head>
-    <link rel="icon" href={favicon} />
-</svelte:head>
+<SeoHead title="home" description="Welcome to my website!"/>
 
-{@render children?.()}
+<main class="w-screen h-screen antialiased">
+    {@render children?.()}
+</main>
