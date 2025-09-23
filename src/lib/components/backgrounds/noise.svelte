be59e99a2a9c7eb9639e15b20434dff8ec42bf48
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-09-23 15:18:33 +0200

    fix: Fetch bg image correctly

--- src/lib/components/backgrounds/noise.svelte
+++ src/lib/components/backgrounds/noise.svelte
@@ -1,9 +1,10 @@
-<script lang="ts">
-  import noiseUrl from '$lib/assets/images/noise.svg';
-</script>
-
 <div
-    class="bg-background bg-auto bg-center absolute inset-0 w-full h-full opacity-20 dark:opacity-10 pointer-events-none -z-30"
-    style="background-image: url('{noiseUrl}')"
+    class="bg-background bg-auto bg-center absolute inset-0 w-full h-full opacity-20 pointer-events-none -z-30"
 >
 </div>
+
+<style>
+    div {
+        background-image: url("$lib/assets/images/noise.svg");
+    }
+</style>
