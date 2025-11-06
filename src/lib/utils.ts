6229a2991a4a9a2956c074aeaa81457beb0876e6
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-11-06 11:09:04 +0100

    feat: Create side-nav component, rearrange overall layout

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,12 @@
+import type { ClassValue } from 'clsx';
+
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: Array<ClassValue>) {
+    return twMerge(clsx(inputs));
+}
+
 export function slugify(str: string): string {
     return str
         .toLowerCase()
