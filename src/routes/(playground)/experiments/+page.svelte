8c288c2fa9e611cd4d0d7001c62476c6ac35f69c
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-12-30 13:51:36 +0100

    chore: Create routes

---
+++ src/routes/(playground)/experiments/+page.svelte
@@ -0,0 +1,1 @@
+<h1>This will be for experimenting</h1>
---
+++ src/routes/(site)/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<h1 class="text-2xl font-serif font-medium">Error: {page.status}</h1>
+<span>
+	Looks like something went wrong:
+	{#if page.error}
+		{page.error.message}
+	{:else}
+		{page.status}
+	{/if}
+</span>
