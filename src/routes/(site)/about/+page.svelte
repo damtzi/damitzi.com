2575581e8ff2c54376270bd1b00cac5f7c6c59be
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-12-30 16:32:34 +0100

    feat: Port /about, /hobbies, /socials pages to sveltekit

---
+++ src/routes/(site)/about/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import SeoHead from '$lib/components/seo-head.svelte';
+	import { TypographyH1, TypographyP } from '$lib/components/typography';
+</script>
+
+<SeoHead title="About" description="About me" />
+
+<div class="flex flex-col gap-8">
+	<div class="flex flex-col gap-2">
+		<TypographyH1>About me</TypographyH1>
+
+		<TypographyP>
+			I am a frontend engineer based in Gdynia, specializing in building and maintaining user
+			interfaces for websites and web applications. Currently, I'm working on
+			<a
+				href="https://klave.com/"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="text-gray-400 transition-colors duration-150 ease-out hover:text-pure-blue hover:cursor-pointer"
+			>
+				Klave
+			</a>
+			at
+			<a
+				href="https://secretarium.com/"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="text-gray-400 transition-colors duration-150 ease-out hover:text-pure-blue hover:cursor-pointer"
+			>
+				Secretarium
+			</a>, crafting intuitive and secure frontends that prioritise privacy and security at every
+			step.
+		</TypographyP>
+		<TypographyP>WIP</TypographyP>
+	</div>
+</div>
---
+++ src/routes/(site)/hobbies/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import SeoHead from '$lib/components/seo-head.svelte';
+	import { TypographyH1, TypographyP } from '$lib/components/typography';
+</script>
+
+<SeoHead title="Hobbies" description="My hobbies and interests" />
+
+<div class="flex flex-col gap-2">
+	<TypographyH1>Hobbies</TypographyH1>
+	<TypographyP>
+		I am a passionate basketball fan, keeping up with the NBA closely. My favorite team is the
+		Chicago Bulls, with the New York Knicks being a close second. I also support Arka Gdynia in the
+		Polish Basketball League - PLK. When I'm not watching hoops, I love playing padel, lifting
+		weights, going for runs, and sailing. I enjoy spending time in the kitchen - I bake my own
+		sourdough <a
+			href="/bread"
+			class="text-gray-400 transition-colors duration-150 ease-out hover:text-pure-blue hover:cursor-pointer"
+			>bread</a
+		>
+		at least once a week. Music is a big passion too; I'm always curating my "<a
+			href="/music/top-picks"
+			class="text-gray-400 transition-colors duration-150 ease-out hover:text-pure-blue hover:cursor-pointer"
+			>top picks</a
+		>" of the year lists and adding new vinyl records to my
+		<a
+			href="/music/vinyls"
+			class="text-gray-400 transition-colors duration-150 ease-out hover:text-pure-blue hover:cursor-pointer"
+			>collection</a
+		>.
+	</TypographyP>
+</div>
