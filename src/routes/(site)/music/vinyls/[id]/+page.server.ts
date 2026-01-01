18209f39b3f6ea783ae5b328b5fb9d9fb7dfefcc
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2026-01-01 14:42:35 +0100

    chore: Opt out of prerendering vinyl routes

--- src/routes/(site)/music/vinyls/[id]/+page.server.ts
+++ src/routes/(site)/music/vinyls/[id]/+page.server.ts
@@ -1,31 +1,31 @@
-import type { PageServerLoad, EntryGenerator } from './$types';
-import type { CollectionsResponse, DiscogsRelease } from '$lib/types';
+import type { PageServerLoad } from './$types';
+import type { DiscogsRelease } from '$lib/types';
 import { error } from '@sveltejs/kit';
 import { DISCOGS_TOKEN } from '$env/static/private';
 
-export const prerender = true;
+// export const prerender = true;
 
 // Provide list of paths to prerender
-export const entries: EntryGenerator = async () => {
-	const response = await fetch(
-		`https://api.discogs.com/users/damitzi__/collection/folders/0/releases?token=${DISCOGS_TOKEN}`,
-		{
-			headers: {
-				'User-Agent': 'damitzi.com/1.0 +https://damitzi.com'
-			}
-		}
-	);
+// export const entries: EntryGenerator = async () => {
+// 	const response = await fetch(
+// 		`https://api.discogs.com/users/damitzi__/collection/folders/0/releases?token=${DISCOGS_TOKEN}`,
+// 		{
+// 			headers: {
+// 				'User-Agent': 'damitzi.com/1.0 +https://damitzi.com'
+// 			}
+// 		}
+// 	);
 
-	const collection: CollectionsResponse = await response.json();
-	const releases = collection.releases;
+// 	const collection: CollectionsResponse = await response.json();
+// 	const releases = collection.releases;
 
-	// Return paths like '/music/vinyl/123'
-	return releases.map((vinyl) => {
-		return {
-			id: `${vinyl.id}`
-		};
-	});
-};
+// 	// Return paths like '/music/vinyl/123'
+// 	return releases.map((vinyl) => {
+// 		return {
+// 			id: `${vinyl.id}`
+// 		};
+// 	});
+// };
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
 	const { id } = params;
