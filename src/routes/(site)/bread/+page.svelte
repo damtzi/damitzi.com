521a5cc683596b39d3056dce1d74b91994277486
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2026-01-01 11:28:31 +0100

    feat: Port the rest of the pages to sveltekit

---
+++ src/routes/(site)/bread/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import SeoHead from '$lib/components/seo-head.svelte';
+	import Breadcrumbs from '$lib/components/breadcrumbs.svelte';
+	import Arrow from '$lib/components/arrow.svelte';
+	import { TypographyH1 } from '$lib/components/typography';
+
+	let { data } = $props();
+</script>
+
+<SeoHead title="Recipes" description="Browse my recipes for bread and other baked goods" />
+
+<div class="flex flex-col gap-2">
+	<TypographyH1>Bread recipes</TypographyH1>
+	<Breadcrumbs />
+	<ul class="space-y-2 w-fit">
+		{#each data.recipes as recipe (recipe.slug)}
+			{@render recipeLink(recipe.title, recipe.slug)}
+		{/each}
+	</ul>
+</div>
+
+{#snippet recipeLink(title: string, slug: string)}
+	<li class="flex items-center group gap-2">
+		<Arrow />
+		<a
+			href={`/bread/${slug}`}
+			class="text-gray-400 transition-colors duration-150 ease-out hover:text-pure-blue hover:cursor-pointer"
+		>
+			{title}
+		</a>
+	</li>
+{/snippet}
---
+++ src/routes/(site)/music/top-picks/[slug]/+page.ts
@@ -0,0 +1,16 @@
+import { error } from '@sveltejs/kit';
+
+export async function load({ params }) {
+    try {
+        const topPick = await import(`../../../../../lib/assets/data/top-picks/${params.slug}.md`);
+
+        return {
+            content: topPick.default,
+            meta: topPick.metadata
+        };
+    }
+    catch (e) {
+        console.error(e);
+        error(404, `Could not find ${params.slug}`);
+    }
+}
