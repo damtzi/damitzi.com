6b92ef2d1999bc2bfcd0a3e151561499eb2c19b6
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2026-06-01 16:06:07 +0200

    feat: Create astro components

---
+++ src/components/typography/index.ts
@@ -0,0 +1,5 @@
+export { default as TypographyH1 } from './h1.astro';
+export { default as TypographyH2 } from './h2.astro';
+export { default as TypographyH3 } from './h3.astro';
+export { default as TypographyH4 } from './h4.astro';
+export { default as TypographyP } from './p.astro';
