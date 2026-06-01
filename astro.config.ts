545010988a491c518aaf8a9279faf5eb47a91219
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2026-06-01 15:06:35 +0200

    chore: Format files

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,20 @@
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  site: "https://damitzi.com",
+	output: 'static',
+	site: 'https://damitzi.com',
+	integrations: [react(), mdx()],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	adapter: cloudflare({
+		imageService: 'compile'
+	}),
 
-  integrations: [react(), mdx()],
-  adapter: cloudflare({
-    imageService: "compile",
-  }),
+	vite: {
+		plugins: [tailwindcss()]
+	}
 });
