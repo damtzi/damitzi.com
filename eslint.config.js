8210a606bdedd8a3f673c480de33dc0f17a2a2e6
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2026-06-01 15:06:10 +0200

    chore: Setup husky, eslint, install packages

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import { includeIgnoreFile } from '@eslint/compat';
+import { fileURLToPath } from 'node:url';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import ts from 'typescript-eslint';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default defineConfig(
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...eslintPluginAstro.configs.recommended,
+	prettier,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		},
+		plugins: {
+			unicorn: eslintPluginUnicorn
+		},
+		rules: {
+			'no-console': ['warn'],
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'unicorn/filename-case': [
+				'error',
+				{
+					case: 'kebabCase',
+					ignore: ['README.md']
+				}
+			],
+			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+			'no-undef': 'off'
+		}
+	}
+);
