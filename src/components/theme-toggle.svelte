fcb0d3e05ed7d1b6561d40983413236f5cd5f430
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2026-07-24 18:34:16 +0200

    style: Switch icons, move theme picker to the footer

--- src/components/theme-toggle.svelte
+++ src/components/theme-toggle.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { Moon, Sun } from '@lucide/svelte';
+	import { HugeiconsIcon } from '@hugeicons/svelte';
+	import { Moon02Icon, Sun01Icon } from '@hugeicons/core-free-icons';
 
 	const toggleTheme = () => {
 		const isDark = document.documentElement.classList.toggle('dark');
@@ -11,11 +12,19 @@
 	type="button"
 	onclick={toggleTheme}
 	aria-label="Toggle Theme"
-	class="relative hidden cursor-pointer sm:block"
+	class="relative cursor-pointer"
 >
-	<Sun class="size-6 scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90" />
-	<Moon
-		class="absolute inset-0 size-6 scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
+	<HugeiconsIcon
+		icon={Sun01Icon}
+		size={16}
+		strokeWidth={2}
+		class="size-4 scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
+	/>
+	<HugeiconsIcon
+		icon={Moon02Icon}
+		size={16}
+		strokeWidth={2}
+		class="absolute inset-0 size-4 scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
 	/>
 	<span class="sr-only">Toggle theme</span>
 </button>
